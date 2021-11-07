4b4f3fa1eb031739ff3b5ecaf19e6a90ff2f9621
Teya <Teya@TVOROG>
2021-11-07 21:18:11 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/2.2 3a.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите двузначное число");
+            string x = Console.ReadLine();
+            char a = '7';
+            if (x.Contains('3') || x.Contains(a))
+                Console.WriteLine("Число " + x + " с цифрой 3 и/или 7");
+            else
+                Console.WriteLine("Ошибка");
+
+        }
+    }
+}
